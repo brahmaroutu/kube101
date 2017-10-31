1262de52089526280281bcddb8fc84d8f30439ae
Morgan Bauer <mbauer@us.ibm.com>
2017-10-30 18:32:36 -0700

    add hostname and change to dockerhub

--- web/app.py
+++ web/app.py
@@ -1,13 +1,14 @@
 from flask import Flask
 from redis import Redis
 import os
+import platform
 app = Flask(__name__)
 redis = Redis(host=os.environ.get('REDIS_HOST', 'redis'), port=6379)
 
 @app.route('/')
 def hello():
     redis.incr('hits')
-    return 'Hello Container World! I have been seen %s times.\n' % redis.get('hits')
+    return 'Hello Container World from %s! I have been seen %s times.\n' %  ( platform.node(), redis.get('hits') )
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
